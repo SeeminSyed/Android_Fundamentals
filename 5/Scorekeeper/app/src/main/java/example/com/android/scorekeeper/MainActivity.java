dec0b499eb2f31ffd9e10713ba1888e133b98108
seeminsyed <seemin711@gmail.com>
2019-03-22 14:09:15 -0400

    added some old projects

---
+++ 5/Scorekeeper/app/src/main/java/example/com/android/scorekeeper/MainActivity.java
@@ -0,0 +1,81 @@
+package example.com.android.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Member variables for holding the score
+    private int mScore1;
+    private int mScore2;
+    TextView mScoreText1;
+    TextView mScoreText2;
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+
+        int viewID = view.getId();
+
+        switch(viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    /**
+     * Method that handles the onClick of both the increment buttons
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        //Get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID){
+            //If it was on Team 1
+            case R.id.increaseTeam1:
+                //Increment the score and update the TextView
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.increaseTeam2:
+                //Increment the score and update the TextView
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+}
